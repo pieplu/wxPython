0117d1c7c965543b2f7958cf45f6ab9aaa5df491
Pieplu <alexis.pieplu@gmail.com>
2014-04-16 19:33:39 -0400

    test7 + import utf8 string

--- test7.py
+++ test7.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
-
 import codecs
 import re
 import sqlite3
 import wx
 import wx.dataview as dv
-
 
 
 #### Fonctions ##########
@@ -18,33 +16,26 @@ import wx.dataview as dv
 def extraction_db(c):
 
     for row in c.execute("SELECT * FROM syllabes"):
-        while True:
-            row = c.fetchone()
-            if row == None:
-                break
-            print row[0].encode('utf-8'), row[1], row[2]
+        row = c.fetchone()
+        if row:
+            print row[0].encode('utf-8'), row[1]
     for row in c.execute("SELECT * FROM syllabes_precedentes"):
-        while True:
-            row = c.fetchone()
-            if row == None:
-                break
+        row = c.fetchone()
+        if row:
             print row[0].encode('utf-8'), row[1].encode('utf-8'), row[2]
     for row in c.execute("SELECT * FROM syllabes_suivantes"):
-        while True:
-            row = c.fetchone()
-            if row == None:
-                break
+        row = c.fetchone()
+        if row:
             print row[0].encode('utf-8'), row[1].encode('utf-8'), row[2]
     return None
 
 
 
 
-
-
 def lecture_syllabes(c, fichier):
 
-    c.execute("CREATE TABLE IF NOT EXISTS syllabes(syllabe TEXT, frequence INT, structure TEXT)")
+
+    c.execute("CREATE TABLE IF NOT EXISTS syllabes(syllabe TEXT, frequence INT)")
     c.execute(
         "CREATE TABLE IF NOT EXISTS syllabes_precedentes (syllabe_1 TEXT, syl_precedente TEXT, frequence_paire INT)")
     c.execute("CREATE TABLE IF NOT EXISTS syllabes_suivantes (syllabe_2 TEXT, syl_suivante TEXT, freq_paire INT)")
@@ -84,9 +75,9 @@ def lecture_syllabes(c, fichier):
     syllabes_triees = sorted(dico_syllabes, key=dico_syllabes.get, reverse=True)
     syllabes_tuples = []
     for syl in syllabes_triees:
-        syllabes_tuples.append((syl, dico_syllabes[syl], "null"))
+        syllabes_tuples.append((syl, dico_syllabes[syl]))
 
-    c.executemany("INSERT INTO syllabes VALUES(?, ?, ?)", syllabes_tuples)
+    c.executemany("INSERT INTO syllabes VALUES(?, ?)", syllabes_tuples)
     syllabes_prec_triees = sorted(dico_syl_prec, key=dico_syl_prec.get, reverse=True)
     syllabes_prec_tup = []
 
@@ -95,8 +86,6 @@ def lecture_syllabes(c, fichier):
             (syllabes_prec_triees[i][0], syllabes_prec_triees[i][1], dico_syl_prec[syllabes_prec_triees[i]]))
 
     c.executemany("INSERT INTO syllabes_precedentes VALUES(?, ?, ?)", syllabes_prec_tup)
-    #for i in range(len(syllabes_prec_tup)):
-    #    print syllabes_prec_tup[i][0].encode('utf-8'), syllabes_prec_tup[i][1].encode('utf-8'), syllabes_prec_tup[i][2]
     syllabes_suiv_triees = sorted(dico_syl_suiv, key=dico_syl_suiv.get, reverse=True)
     syllabes_suiv_tup = []
 
@@ -105,17 +94,13 @@ def lecture_syllabes(c, fichier):
             (syllabes_suiv_triees[i][0], syllabes_suiv_triees[i][1], dico_syl_suiv[syllabes_suiv_triees[i]]))
 
     c.executemany("INSERT INTO syllabes_suivantes VALUES(?, ?, ?)", syllabes_suiv_tup)
-    #for i in range(len(syllabes_suiv_tup)):
-    #    print syllabes_suiv_tup[i][0].encode('utf-8'), syllabes_suiv_tup[i][1].encode('utf-8'), syllabes_suiv_tup[i][2]
 
 ### Optimisation avec Sylvain -----------------
 def fais_le(c, tbl, transforms):
     tr = transforms
     for row in c.execute("SELECT * FROM %s"%tbl):
-        while True:
-            row = c.fetchone()
-            if row == None:
-                break
+        row = c.fetchone()
+        if row:
             yield [tr[i](row[i]) for i in range(2)]
 
 # Une fonction qui convertit une string, l'autre pas
@@ -125,10 +110,8 @@ ff, fn = lambda s: s.encode('utf-8'), lambda s: s
 
 def search(c,syl):
     for ligne in c.execute("SELECT * FROM syllabes WHERE syllabe = '%s'"%syl):
-        while True:
-            ligne = c.fetchone()
-            if ligne == None:
-                break
+        ligne = c.fetchone()
+        if ligne:
             yield ligne[0].encode('utf-8'), str(ligne[1])
 
 
@@ -136,14 +119,14 @@ def search(c,syl):
 ### PROGRAMME
 
 
-connection = sqlite3.connect('basedonnees.db')
+connection = sqlite3.connect('basededonnees.db')
 c = connection.cursor()
 
 lecture_syllabes(c, '010_C7.xml')
 connection.commit()
 
-
-
+result = search(c, "qu")
+print result
 
 #extraction_db(c)
 
@@ -151,7 +134,7 @@ connection.commit()
 liste_test = []
 
 for syl, nb in fais_le(c, tbl='syllabes', transforms=(ff,fn)):
-        liste_test.append([syl, str(nb)])
+        liste_test.append([syl, unicode(nb)])
 
 
 print len(liste_test) #la bd est complètement rechargé à chaque execution, à voir
@@ -160,62 +143,96 @@ print len(liste_test) #la bd est complètement rechargé à chaque execution, à
 
 ### Interface
 
-class ExamplePanel(wx.Panel):
+
+
+
+
+class TestLayoutConstraints(wx.Panel):
     def __init__(self, parent):
-        #extraction_syllabes(c)
+        wx.Panel.__init__(self, parent, -1)
+        self.SetAutoLayout(True)
 
 
-        wx.Panel.__init__(self, parent)
-        #self.quote = wx.StaticText(self, label="Your quote :", pos=(20, 30))
+        self.panelA = wx.Window(self, -1, style=wx.SIMPLE_BORDER)
 
-        # A multiline TextCtrl - This is here to show how the events work in this program, don't pay too much attention to it
-        self.logger = wx.TextCtrl(self, pos=(400,20), size=(150,60), style=wx.TE_MULTILINE | wx.TE_READONLY)
+        txt = wx.StaticText(
+                    self.panelA, -1,
+                    "Recherche",
+                    (20,20), (-1, 50)
+                    )
 
 
+        lc = wx.LayoutConstraints()
+        lc.top.SameAs(self, wx.Top, 0)
+        lc.left.SameAs(self, wx.Left, 0)
+        lc.bottom.PercentOf(self, wx.Bottom, 35)
+        lc.right.SameAs(self, wx.Right, 0)
+        self.panelA.SetConstraints(lc)
+        
+        self.panelA.logger = wx.TextCtrl(self, pos=(400,20), size=(150,60), style=wx.TE_MULTILINE | wx.TE_READONLY)
+        self.panelA.editname = wx.TextCtrl(self, value="...", pos=(110, 20), size=(140,-1))
+        self.panelA.button =wx.Button(self, label="Rechercher", pos=(260, 17))        
 
-        # the edit control - one line version.
-       # self.lblname = wx.StaticText(self, label="Your name :", pos=(20,60))
-       # self.editname = wx.TextCtrl(self, value="Enter here your name", pos=(150, 60), size=(140,-1))
-
-        self.dvlc = dvlc = dv.DataViewListCtrl(self, size=(200,200), pos=(0,100))
-
-        dvlc.AppendTextColumn('Sylabe', width=70)
-        dvlc.AppendTextColumn('Occurence', width=70)
-
+        self.panelB = wx.Window(self, -1, style=wx.SIMPLE_BORDER)
+        lc = wx.LayoutConstraints()
+        lc.top.Below(self.panelA, 0)
+        lc.right.PercentOf(self, wx.Right, 34)
+        lc.bottom.SameAs(self, wx.Bottom, 0)
+        lc.left.SameAs(self, wx.Left, 0)
+        self.panelB.SetConstraints(lc)
+        
+        self.panelB.dvlc = dvlc = dv.DataViewListCtrl(self.panelB, size=(200,200), pos=(0,0))
+        
+        dvlc.AppendTextColumn('Syllabe', width=75)
+        dvlc.AppendTextColumn('Occurrence', width=75)
+        
         for itemvalues in liste_test:
             dvlc.AppendItem(itemvalues)
+            
 
 
-        #ajout à la liste du tableau
+        self.panelC = wx.Window(self, -1, style=wx.SIMPLE_BORDER)
+        lc = wx.LayoutConstraints()
+        lc.top.Below(self.panelA, 0)
+        lc.right.PercentOf(self, wx.Right, 67)
+        lc.bottom.SameAs(self, wx.Bottom, 0)
+        lc.left.RightOf(self.panelB, 0)
+        self.panelC.SetConstraints(lc)
+        
+        wx.StaticText(
+            self.panelC, -1, "Paires syllabes précédentes", (4, 4)
+            )
+         
+        self.panelD = wx.Window(self, -1, style=wx.SIMPLE_BORDER)
+        wx.StaticText(
+            self.panelD, -1, "Paires syllabes suivantes", (4, 4)
+            )
+        
+        lc = wx.LayoutConstraints()
+        lc.bottom.SameAs(self, wx.Bottom, 0)
+        lc.right.SameAs(self, wx.Right, 0)
+        lc.left.RightOf(self.panelC, 0)
+        lc.top.Below(self.panelA, 0)
+        self.panelD.SetConstraints(lc)
 
 
-        #the edit control - one line version.
-        self.lblname = wx.StaticText(self, label="Recherche :", pos=(20,20))
-        self.editname = wx.TextCtrl(self, value="de", pos=(110, 20), size=(140,-1))
-        # A button
-        self.button =wx.Button(self, label="Rechercher", pos=(260, 20))
-        self.Bind(wx.EVT_BUTTON, self.OnClick,self.button)
+        
+        self.Bind(wx.EVT_BUTTON, self.OnClick,self.panelA.button)
 
-        self.buttonTake =wx.Button(self, label="Prendre", pos=(260, 50))
-        self.Bind(wx.EVT_BUTTON, self.selectElem,self.buttonTake)
-
-
-
-
-
-
+        
     def OnClick(self,event):
-        str_recherche = self.editname.GetValue()
-        tab_search = search(c,str_recherche)
-        for itemvalues in tab_search:
-            self.dvlc.AppendItem(itemvalues)
-            self.logger.AppendText('item: %s\n' % itemvalues[0])
+        self.panelA.logger.AppendText('Evtclick: %s\n' % event.GetString())
 
-    def selectElem(self,event):
-        id = self.dvlc.GetSelectedRow()
-        val = self.dvlc.GetTextValue(id, 0)
-        self.logger.AppendText('item: %s\n' % val)
 
+
+
+
+    #def OnClick(self,event):
+    #    str_recherche = self.editname.GetValue()
+    #    tab_search = search(c,str_recherche)
+    #    for itemvalues in tab_search:
+    #        self.dvlc.AppendItem(itemvalues)
+    #
 
 
 
@@ -225,14 +242,10 @@ class MyFrame(wx.Frame):
         wx.Frame.__init__(self, parent, title=title, size=(600,400))
 
 app = wx.App(False)
-frame = MyFrame(None, 'Appli')
-panel = ExamplePanel(frame)
+frame = MyFrame(None, 'Application syllabes')
+panel = TestLayoutConstraints(frame)
 frame.Show()
 app.MainLoop()
-
-
-
-
 
 
 
